be86404b8f3d4c0b3733cf1f05b6797c9a6e9c4d
Rankshow <uwaomaobinna20@gmail.com>
2024-06-25 19:23:32 +0100

    Debugs the error in the program class

--- BackgroundJobConsole/Program.cs
+++ BackgroundJobConsole/Program.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BackgroundJobConsole;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+ 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
-        services.AddHostedService<BackgroundService>()
+        services.AddHostedService<BackgroundJob>()
         .AddLogging(configure => configure.AddConsole());
     }).Build();
 
