63ca5f664f697667228afc16dfe2829e01a80ed0
Rankshow <uwaomaobinna20@gmail.com>
2024-06-25 18:45:42 +0100

    Implements the IHostedService and IDisposable in the backgroundJob class.

--- BackgroundJobConsole/BackgroundJob.cs
+++ BackgroundJobConsole/BackgroundJob.cs
@@ -1,21 +1,38 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 
 namespace BackgroundJobConsole;
 
 internal class BackgroundJob : IHostedService, IDisposable
 {
-
+    readonly ILogger<BackgroundJob> _logger;
+    private Timer _timer;
+    public BackgroundJob(ILogger<BackgroundJob> logger)
+    {
+        _logger = logger;
+    }
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("Starting background service.....");
+        _timer = new(ShoutMessage, null, TimeSpan.Zero, TimeSpan.FromSeconds(8));
+        return Task.CompletedTask;  
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation($"Stopping the background service");
+        _timer.Change(Timeout.Infinite, 0);
+        return Task.CompletedTask;
     }
     public void Dispose()
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("Disposing timer object...");
+        _timer.Dispose();
+    }
+
+    private async void ShoutMessage(object? state)
+    {
+        Console.WriteLine("Happy birthday Gabriel, may lines fall in your path....");
     }
 }
