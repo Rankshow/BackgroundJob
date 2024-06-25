b6cc229c0467050c57077026f5933d1b95c6949a
Rankshow <uwaomaobinna20@gmail.com>
2024-06-25 18:19:16 +0100

    Project setup

---
+++ BackgroundJobConsole/BackgroundJob.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace BackgroundJobConsole;
+
+internal class BackgroundJob : IHostedService, IDisposable
+{
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
